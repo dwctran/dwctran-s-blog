bc4b040bd01d5236a34cc29abbaff746f38110ff
dwctran <dwktheda@gmail.com>
2021-12-28 17:23:42 +0700

    first-commit

---
+++ components/LayoutWrapper.js
@@ -0,0 +1,54 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import SectionContainer from './SectionContainer'
+import Footer from './Footer'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+
+const LayoutWrapper = ({ children }) => {
+  return (
+    <SectionContainer>
+      <div className="flex flex-col justify-between h-screen ">
+        <header className="fixed z-10 top-0 left-0 right-0 px-40 bg-white dark:bg-[#171717] dark:border-[#f5f5f5] flex items-center justify-between py-10 border-b border-black">
+          <div>
+            <Link href="/" aria-label="Tailwind CSS Blog">
+              <div className="flex items-center justify-between">
+                <div className="mr-3">
+                  <Logo />
+                </div>
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
---
+++ data/authors/default.md
@@ -0,0 +1,14 @@
+---
+name: Duc Tran
+avatar: /static/images/Untitled-1.png
+occupation: Economics Undergraduate
+company: Foreign Trade University
+email: dinhductran189@gmail.com
+twitter: https://twitter.com/Twitter
+linkedin: https://www.linkedin.com
+github: https://github.com
+---
+
+Duc Tran is a university sophomore, studying under Economics Major. His real interests lay on **Data Analytics, UI/UX Design and Web Development.**
+
+In his own time, he learned to conducted projects on data analysis, reading tech posts, playing the guitar, or joining virtual poker games. He has been gradually accumulating skillsets on data manipulation and visualization. And building data model with machine learning is a challenge he set to himself on his learning journey.
